0339b7c14f2c01897db76ca56d7b1d833fb5126f
El1te <72935319+El1tee@users.noreply.github.com>
2022-02-12 20:13:01 +0200

    Update sv_announcements.lua

--- server/sv_announcements.lua
+++ server/sv_announcements.lua
@@ -1,41 +1,43 @@
-ESX = nil
+local ESX = exports["es_extended"]:getSharedObject()
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
--- Start of the config.
-local command = 'ptwt' -- You can change the command here.
-local minjobgrade = 1 -- You can change the minimium job grade allowed to send the messages.
-local enablephone = false -- change to true if you want have phone required to send announcements.
--- End of the config.
-
-TriggerEvent('es:addCommand', command, function(source, args, user)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local text    = nil -- Don't touch this.
-    --
-    if (xPlayer.job.name == 'police' and xPlayer.job.grade >= minjobgrade) then
-        text = 'Police'
-    elseif (xPlayer.job.name == 'ambulance' and xPlayer.job.grade >= minjobgrade) then
-        text = 'EMS'
-    elseif (xPlayer.job.name == 'mecano' and xPlayer.job.grade >= minjobgrade or xPlayer.job.name == 'mechanic' and xPlayer.job.grade >= minjobgrade) then
-        text = 'Mechanic'
-    elseif (xPlayer.job.name == 'taxi' and xPlayer.job.grade >= minjobgrade) then
-        text = 'Taxi'
-    end
-    if (enablephone) then
-        if (xPlayer.getInventoryItem('phone').count >= 1) then
-            if (text) then
-                TriggerClientEvent('chat:addMessage', -1, {
-                    template = '<div style="padding: 0.5vw; margin: 0.5vw; background: -webkit-linear-gradient(right, transparent 15%, rgba(5, 5, 5, 0.4) 85%); border-radius: 7px;"><i class="fa fa-user"></i> {0}:  {1}</div>',
-                    args = {"["..text.."]", table.concat(args, " ")}
-                })
-            end
-        end
-    else
-        if (text) then
-            TriggerClientEvent('chat:addMessage', -1, {
-                template = '<div style="padding: 0.5vw; margin: 0.5vw; background: -webkit-linear-gradient(right, transparent 15%, rgba(5, 5, 5, 0.4) 85%); border-radius: 7px;"><i class="fa fa-user"></i> {0}:  {1}</div>',
-                args = {"["..text.."]", table.concat(args, " ")}
-            })
-        end
-    end
-end)
+if not ESX then
+	SetTimeout(3000, function() print('[^3WARNING^7] Unable to start '..GetCurrentResourceName()..' - server ESX object not found') end)
+else
+    -- Start of the config.
+	local Config = {
+		Command = 'ptwt', -- You can change the command here.
+		MinJobGrade = 1, -- You can change the minimium job grade allowed to send the messages.
+		EnablePhone = false -- change to true if you want have phone required to send announcements.
+	}
+    -- End of the config.
+	RegisterCommand(Config.Command, function(source, args, rawCommand)
+		local xPlayer = ESX.GetPlayerFromId(source)
+		local text    = nil
+		if (xPlayer.job.name == 'police' and xPlayer.job.grade >= Config.MinJobGrade) then
+			text = 'Police'
+		elseif (xPlayer.job.name == 'ambulance' and xPlayer.job.grade >= Config.MinJobGrade) then
+			text = 'EMS'
+		elseif (xPlayer.job.name == 'mecano' and xPlayer.job.grade >= Config.MinJobGrade or xPlayer.job.name == 'mechanic' and xPlayer.job.grade >= Config.MinJobGrade) then
+			text = 'Mechanic'
+		elseif (xPlayer.job.name == 'taxi' and xPlayer.job.grade >= Config.MinJobGrade) then
+			text = 'Taxi'
+		end
+		if (Config.EnablePhone) then
+			if (xPlayer.getInventoryItem('phone').count >= 1) then
+				if (text) then
+					TriggerClientEvent('chat:addMessage', -1, {
+						template = '<div style="padding: 0.5vw; margin: 0.5vw; background: -webkit-linear-gradient(right, transparent 15%, rgba(5, 5, 5, 0.4) 85%); border-radius: 7px;"><i class="fa fa-user"></i> {0}:  {1}</div>',
+						args = {"["..text.."]", table.concat(args, " ")}
+					})
+				end
+			end
+		else
+			if (text) then
+				TriggerClientEvent('chat:addMessage', -1, {
+					template = '<div style="padding: 0.5vw; margin: 0.5vw; background: -webkit-linear-gradient(right, transparent 15%, rgba(5, 5, 5, 0.4) 85%); border-radius: 7px;"><i class="fa fa-user"></i> {0}:  {1}</div>',
+					args = {"["..text.."]", table.concat(args, " ")}
+				})
+			end
+		end
+	end)
+end
